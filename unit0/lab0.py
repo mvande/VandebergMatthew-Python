ec11cb4b49fb3b85d6f23ed106b909dc20d3a60a
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2020-09-03 10:11:34 -0700

    Initial commit

---
+++ unit0/lab0.py
@@ -0,0 +1,6 @@
+def main():
+    # the following code prints using the print statement
+    print("hello tacoma - how are you today?")
+    print("")
+    print("my new favorite class is Python")
+main()
