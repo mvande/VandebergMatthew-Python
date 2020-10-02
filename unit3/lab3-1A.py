efe29382f1f34ac399a812229da6537411134b93
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2020-10-01 18:02:18 -0700

    Starting and finishing labs 3-1A and 3-1B

---
+++ unit3/lab3-1A.py
@@ -0,0 +1,25 @@
+#Matthew Vandeberg
+#Python
+#Period 2
+
+def main():
+    print("draw three boxes \n")
+    drawBoxes()
+    print()
+    drawBoxes()
+    print()
+    drawBoxes()
+
+def drawTopBot():
+    print("******")
+
+def drawSides():
+    print("|    |")
+
+def drawBoxes():
+    drawTopBot()
+    drawSides()
+    drawSides()
+    drawTopBot()
+
+main()
---
+++ unit3/lab3-1B.py
@@ -0,0 +1,24 @@
+#Matthew Vandeberg
+#Python
+#Period 2
+
+def main():
+    goTeam()
+    print()
+    theBest()
+    print()
+    theBest()
+    print()
+    goTeam()
+
+def goTeam():
+    print("Go, team, go!")
+    print("You can do it.")
+
+def theBest():
+    goTeam()
+    print("You are the best,")
+    print("In the West.")
+    goTeam()
+
+main()
