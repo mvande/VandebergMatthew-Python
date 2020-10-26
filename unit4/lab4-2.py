2219ca4c088c55dbffeccd7551213084ee3973b4
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2020-10-26 10:35:47 -0700

    Probably finished lab4-2

---
+++ unit4/lab4-2.py
@@ -0,0 +1,20 @@
+#Matthew Vandeberg
+#Python
+#Period 2
+
+def main():
+    #Create loop from 2 to 6 that prints 2 through 6 and the product of each number and ten.
+    for i in range(2, 7):
+        print("index " + str(i) + " - " + str(i * 10))
+
+    #Create list of fruits and pass it to the pluralize function.
+    fruits = ["banana", "apple", "orange", "grape"]
+    pluralize(fruits)
+
+#Loops through the passed list and adds an "s" to each string in the list and then prints the modified list.
+def pluralize(list):
+    for i in range(0, len(list)):
+        list[i] += "s"
+    print(str(list))
+
+main()
