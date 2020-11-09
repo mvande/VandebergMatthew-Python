39e014355783516cfe5baf42cd91c8cb5d494bf0
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2020-11-09 10:13:11 -0800

    Finished and submitted lab4-3

--- unit4/lab4-3.py
+++ unit4/lab4-3.py
@@ -7,17 +7,17 @@ def main():
 
     print()
 
-    incTriangle()
+    increaseTri()
 
 #Uses nested for loops to print a 7x7 square of * characters.
 def draw7():
-    for i in range(7): #Loops from 0 to 6.
-        for j in range(7): #Loops from 0 to 6.
+    for i in range(7): #Loops from 0 to 6. Determines height of square.
+        for j in range(7): #Loops from 0 to 6. Determines width of square.
             print('* ', end='') #Prints '* '. end='' changes what prints after '* ' from '\n' to ''.
         print() #Moves to a new line.
     
 #Uses nested for loops to print a stack of numbers 1 to 7.
-def incTriangle():
+def increaseTri():
     for i in range(1, 8): #Loops from 1 to 7. This loop keeps track of the number to be printed in each line.
         for j in range(i): #Loops from 0 to i. This loop determines how many numbers are printed per line.
             print(i, end='') #Prints the value of i from the outer loop and changes the end character to ''.
