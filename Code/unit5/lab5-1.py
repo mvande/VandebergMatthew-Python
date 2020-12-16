d7cb3ca643e361f41c96ceed51f091f6f1b84034
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2020-12-16 10:14:19 -0800

    Finished and commented lab5-1 and lab5Classes (pre lab5-2)

--- Code/unit5/lab5-1.py
+++ Code/unit5/lab5-1.py
@@ -1,24 +1,34 @@
-from Code.unit5.lab5Classes import *
+#Matthew Vandeberg
+#Python
+#Period 2
+
+from Code.unit5.lab5Classes import * #Import everything from lab5Classes file
 
 def main():
-    myPet1 = pet('dog', 'Hanz', 'german shepard')
-    print(myPet1.name)
-    print(myPet1.petType)
-    myPet1.whatIsIt()
+    myPet1 = pet('dog', 'Hanz', 'german shepard') #Create a new pet with the required info
+    print(myPet1.name) #Print pet name
+    print(myPet1.petType) #Print what type the pet is (caged or not)
+    myPet1.whatIsIt() #Print all the info of the pet
 
-    myPet2 = pet('cat', 'that one', 'feral')
-    print(myPet2.name)
-    print(myPet2.petType)
-    myPet2.whatIsIt()
+    print()
 
-    myCage1 = cage('snake', True)
-    print(myCage1.type)
-    print(myCage1.petType)
-    myCage1.whatDanger()
+    myPet2 = pet('cat', 'Franz', 'pixie-bob') #Create a new pet with the required info
+    print(myPet2.name) #Print pet name
+    print(myPet2.petType) #Print what type the pet is (caged or not)
+    myPet2.whatIsIt() #Print all the info of the pet
 
-    myCage2 = cage('rat', False)
-    print(myCage2.type)
-    print(myCage2.petType)
-    myCage2.whatDanger()
+    print()
+
+    myCage1 = cage('snake', True) #Create a new caged pet with proper parameters
+    print(myCage1.type) #Print the identity of the pet
+    print(myCage1.petType) #Print what type the pet is (caged or not)
+    myCage1.whatDanger() #Print whether the pet is dangerous or not
+
+    print()
+
+    myCage2 = cage('rat', False) #Create a new caged pet with required parameters
+    print(myCage2.type) #Print the identity of the pet
+    print(myCage2.petType) #Print what type the pet is (caged or not)
+    myCage2.whatDanger() #Print whether the pet is dangerous or not
 
 main()
--- Code/unit5/lab5Classes.py
+++ Code/unit5/lab5Classes.py
@@ -1,21 +1,29 @@
+#Matthew Vandeberg
+#Python
+#Period 2
+
 class pet():
+    #List out and assign the pet class's parameters and attributes 
     def __init__(self, type, name, breed):
         self.type = type
         self.name = name
         self.breed = breed
         self.petType = 'cage free pet'
 
+    #Function for pet objects to print out all of their attributes
     def whatIsIt(self):
-        print(self.type + ' ' + self.name + ' ' + self.breed + ' is a ' + self.petType)
+        print(self.type + ' ' + self.name + ' (' + self.breed + ') is a ' + self.petType)
 
 class cage():
+    #List out and assign the cage class's parameters and attributes
     def __init__(self, type, danger):
         self.type = type
         self.danger = danger
         self.petType = 'caged pet'
 
+    #Function for cage objects to print out the danger of the caged pet
     def whatDanger(self):
         if self.danger:
-            print('danger')
+            print('this pet is dangerous')
         else:
-            print('not danger')
+            print('this pet is not dangerous')
